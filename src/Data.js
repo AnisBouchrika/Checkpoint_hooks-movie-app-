c75c2041935312aa367696ed4a7924c0129dea17
AnisBouchrika <anice.bouchrika@gmail.com>
2020-11-30 11:49:47 +0100

    last commit

---
+++ src/Data.js
@@ -0,0 +1,37 @@
+export const moviesData = [
+    {
+        id: Math.random(),
+        image: "https://melhoresfilmesdahistoriaemumano.files.wordpress.com/2015/01/220px-shawshankredemptionmovieposter.jpg",
+        rating:5,
+        name: "The Shawshank Redemption",
+        date: "1994"
+    },
+    {
+        id: Math.random(),
+        image: "https://upload.wikimedia.org/wikipedia/en/0/07/The_Wall_%282017_film%29.png",
+        rating:4,
+        name: "The Wall",
+        date: "2017",
+    },
+    {
+        id: Math.random(),
+        image: "https://upload.wikimedia.org/wikipedia/en/5/5a/Blooddiamondposter.jpg",
+        rating:3,
+        name: "Blood diamonds",
+        date: "2006",
+    },
+    {
+        id: Math.random(),
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Accident_Man_film_poster.jpg/220px-Accident_Man_film_poster.jpg",
+        rating:2,
+        name: "Accident man",
+        date: "2018"
+    },
+    {
+        id: Math.random(),
+        image: "https://upload.wikimedia.org/wikipedia/en/1/1f/Dolittle_%282020_film_poster%29.png",
+        rating:1,
+        name: "Dolittle",
+        date: "2020"
+    },
+];
